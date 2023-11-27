88d5ab93bec70cbf940a80a2ca4af41dfa83edfd
Akito-Fujihara <1998922akito@gmail.com>
2023-11-27 13:04:06 +0900

    tf init & vpc

---
+++ terraform/provider.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      Name = "ARC-AutoScalingRunnerScaleSet"
+    }
+  }
+}
+
+terraform {
+  required_version = "~> 1.5.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.1.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
+  }
+}
